eac3340050ab3189d0b66fd64351da918d64f841
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-10-06 15:45:09 +0200

    test9

--- git-hook-stop-tokens.sh
+++ git-hook-stop-tokens.sh
@@ -1,8 +1,8 @@
-FILES_PATTERN='\.txt$'
-FORBIDDEN='hello world'
+FILES_PATTERN='\.bru$'
+FORBIDDEN='eyJ'
 
 git diff --cached --name-only | \
   grep -E "$FILES_PATTERN" | \
   xargs grep --with-filename -n -F "$FORBIDDEN" && \
-  echo "COMMIT REJECTED Found '$FORBIDDEN' references. Please remove them before committing" && exit 1 || \
-  echo "No forbidden text found"
+  echo "❌ rrrr COMMIT STOPPET fant JWT Token i .bru. Inneholder '$FORBIDDEN. Slett dem før du committer" && exit 1 || \
+  echo "✅ rrrr Fant ingen JWT Token i .bru filer"
