6de0e963ceb30e2017cd416f50e372db30cf7e28
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-10-08 11:25:52 +0200

    test11

--- git-hook-stop-tokens.sh
+++ git-hook-stop-tokens.sh
@@ -1,8 +1,9 @@
 FILES_PATTERN='\.bru$'
 FORBIDDEN='eyJ'
 
-git diff --cached --name-only | \
-  grep -E "$FILES_PATTERN" | \
-  xargs grep --with-filename -n -F "$FORBIDDEN" && \
-  echo "❌ rrrr COMMIT STOPPET fant JWT Token i .bru. Inneholder '$FORBIDDEN. Slett dem før du committer" && exit 1 || \
-  echo "✅ rrrr Fant ingen JWT Token i .bru filer"
+git diff --cached --name-only | grep -E "$FILES_PATTERN" | xargs -r grep -q -F "$FORBIDDEN" && {
+  echo "❌ COMMIT STOPPET: JWT Token funnet i .bru filer"
+  exit 1
+}
+
+echo "✅ Ingen JWT Tokens funnet"
